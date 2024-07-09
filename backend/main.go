20531c2e71d647ffcf8911db3bb4f810f23c244e
goncalo2k <goncalo.miranda.cplx@gmail.com>
2024-07-09 19:53:39 +0100

    Initial backend

---
+++ backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"backend/utils"
+
+	"github.com/joho/godotenv"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "bonjour monde")
+}
+
+func main() {
+	godotenv.Load(".env")
+	http.HandleFunc(";", helloHandler)
+	fmt.Println("Server is running on port:", utils.GetEnvVar("PORT"))
+	http.ListenAndServe(utils.GetEnvVar("PORT"), nil)
+}
